e3955bb39f9608cc71621727a0c1a5a21709301f
realitygaps <realitygaps@gapsinreality.com>
2014-11-04 23:01:46 +0100

    Initial commit

---
+++ Extension.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Discourse;
+
+use Bolt;
+
+class Extension extends \Bolt\BaseExtension
+{
+    /**
+     * Extensions PHP name
+     *
+     * @var string
+     */
+    const NAME = "Discourse";
+
+    public function getName()
+    {
+        return Extension::NAME;
+    }
+
+    public function initialize()
+    {
+        if ($this->app['config']->getWhichEnd() == 'frontend') {
+            // Add Twig functions
+            $this->addTwigFunction('discourse', 'twigDiscourse');
+	          $this->addSnippet('endofhead', 'embedjs');
+
+        }
+    }
+
+    /**
+     * Return discourse comments div
+     *
+     * @return \Twig_Markup
+     */
+    public function twigDiscourse()
+    {
+    	$str = "<div id=\"discourse-comments\"></div>";
+        return new \Twig_Markup($str, 'UTF-8');
+    }
+
+    /**
+     * Returns a string with the javascript to embed
+     *
+     * @return String
+     */
+    public function embedjs()
+    {
+      return "<script type='text/javascript'>
+          var discourseUrl = '" . $this->config['discourseurl'] . "';
+                discourseEmbedUrl = window.location();
+
+        (function() {
+              var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+                    d.src = discourseUrl + 'javascripts/embed.js';
+                  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+                })();
+      </script>";
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+Discourse Embedded Comments for Bolt
+====================================
+
+This is a bolt extension to use self hosted comments with your installation of disourse.
+
+Still under heavy development!
+
+Requirements
+------------
+Discourse from https://discourse.org
+
+
+Use
+---
+
+* Define the domain of your discourse install in the config.yml variable discourseurl
+
+* Insert the following twig function in your template where you want the comments diplayed
+
+{{ discourse() }}
